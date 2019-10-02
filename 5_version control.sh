13afac629bef70f541477a74c0333d02bfe597d2
LingFelce <55535650+LingFelce@users.noreply.github.com>
2019-10-02 20:23:12 +0100

    typed up notes on using communal repository

---
+++ 5_version control.sh
@@ -0,0 +1,11 @@
+#Notes on using communal repository:
+git pull #to get latest version
+git branch <name> #create new branch of local repository - do all work on this branch
+git checkout <name> #switch to new branch
+git add
+git commit -m "update comment"
+git push origin <name> #push branch to git hub
+#on github website, select branch and select new pull request. Merge branches and delete branch.
+git checkout master #switch to master branch
+git branch -d <name> #delete new branch
+git pull #pull latest version to local master repository
