bdb10bade444806ac4e27650179c584b13bad52f
LingFelce <55535650+LingFelce@users.noreply.github.com>
2019-10-16 10:38:32 +0100

    created scanpy file

---
+++ 24_scanpy.py
@@ -0,0 +1,8 @@
+"""
+Single Cell RNA Seq in Python (Jupyter Lab)
+
+Launch Jupyter Lab in folder that you want to work from (and in environment that you want to work in - might have to conda install
+Juptyper Lab in each conda environment)
+
+"""
+
