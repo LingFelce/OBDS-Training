799301377fe3eb6705d51f3b750e0e0333c1e68c
LingFelce <55535650+LingFelce@users.noreply.github.com>
2019-10-18 13:17:47 +0100

    created file

---
+++ 26_Pybedtools.py
@@ -0,0 +1,10 @@
+""""""""""""""""""""""
+18th October 2019
+Pybedtools wraps and extends bedtools and offers feature level manipulations from within Python.
+https://daler.github.io/pybedtools/
+
+Will do this in Jupyter Lab, then will export as HTML.
+
+Three brief examples: https://daler.github.io/pybedtools/3-brief-examples.html
+
+"""""""""""""""""""""
