ecd6b0fe2655f29c78a9c5a9c0688a9d6cf63c9a
LingFelce <55535650+LingFelce@users.noreply.github.com>
2019-10-24 11:54:41 +0100

    created file

---
+++ 30_Machine_learning.py
@@ -0,0 +1,12 @@
+"""
+Further intro to machine learning 24th October 2019
+
+Artificial intelligence vs machine learning
+
+2 main classes of algorithms: 
+  supervised (input data + desired outcome)  - linear regression, neural networks, perceptron. Answer a question - right or wrong
+  unsupervised (input data only) - PCA, k-means, t-SNE, UMAP. Dimensionality reduction, visualisation, computational efficiency
+
+Exercises using iris dataset from sklearn, done in Jupyter Lab (export at HTML)
+
+"""
